6d04326508683e6c5be5c16df6ec58769bc647c8
Matt Woods <mjaywoods@gmail.com>
2024-11-01 13:16:44 +0000

    Update Website

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/2048-in-react",
+  output: "export",
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
