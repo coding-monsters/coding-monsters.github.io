0f8535014477cafe50dc8807241a41519429965b
Matt Woods <mjaywoods@gmail.com>
2024-11-01 23:56:17 +0000

    Update Website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+import Image from "next/image";
+import { Inter } from 'next/font/google'
+ 
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Coding Monsters - Under Maintenance
-       </main>
-    </div>
+    <main className={inter.className}>
+      <div className="grid grid-cols-2 gap-4">
+  <div> <Image
+      src="/image.jpg"
+      width={923}
+      height={901}
+      alt="Picture of the author"
+    /></div>
+  
+  <div> <h1 className="text-6xl font-semibold pt-32 pb-4 pl-24">Coming<br /> Soon / <br /><span className="font-light">Coding<br /> Monsters</span></h1>
+  <p className="pl-24">New Website For 2025</p>
+  </div>
+  
+</div>
+</main>
+     
+ 
   );
 }
